7b31374b5986318cdb18e9c8aafeb4bc3b1694f5
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-21 19:23:47 +0530

    roboshop project 1st commit

---
+++ roboshop.sh
@@ -0,0 +1,22 @@
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0063ebc81c5fcfe82"
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "forntend")
+ZONE_ID="Z066140621FS3C4YYVXA1"
+DOMAIN_NAME="tadikondadevops.site"
+
+for instance in $(INSTANCES[@])
+
+do
+
+INTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0063ebc81c5fcfe82
+ --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=chinna}]" --query "Instances[0].PrivateIpAddress" --output text)
+
+if [ $instance != "forntend" ]
+then 
+   IP=$(aws ec2 describe-instances --instance-ids i-0abcdef123456789 --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+ else 
+      IP=$(aws ec2 describe-instances --instance-ids i-0abcdef123456789 --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+
+fi
+   echo " $intance IP address: $IP"
+done
