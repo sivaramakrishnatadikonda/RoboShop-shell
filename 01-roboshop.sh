dcae8a68978412faebb48659e557324c3b28bf8d
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-22 11:43:04 +0530

    RoboShop sample data

--- 01-roboshop.sh
+++ 01-roboshop.sh
@@ -7,7 +7,7 @@ DOMAIN_NAME="tadikondadevops.site"
 for instance in ${INSTANCES[@]}
 do
 
-INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0063ebc81c5fcfe82 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$intance}]" --query "Instances[0].PrivateIpAddress" --output text)
+INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0063ebc81c5fcfe82 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].PrivateIpAddress" --output text)
 
 if [ $instance != "frontend" ]
 then 
