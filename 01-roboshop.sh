a70f947842ad4d73499b64b229b1e30e480b63f1
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2025-05-22 11:59:56 +0530

    RoboShop sample data

--- 01-roboshop.sh
+++ 01-roboshop.sh
@@ -13,10 +13,10 @@ do
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-        RECORD_NAME="$instance.$DOMAIN_NAME"
+       
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-        RECORD_NAME="$DOMAIN_NAME"
+       
     fi
     echo "$instance IP address: $IP"
 
